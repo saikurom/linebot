c4d464977bca1640d452f37f51de35e4474bb7cc
yakitori <142929539+saikurom@users.noreply.github.com>
2023-08-23 13:33:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from linebot import LineBotApi, WebhookHandler 
 from linebot.models import MessageEvent, TextMessage, TextSendMessage 
 from linebot.exceptions import InvalidSignatureError 
